56e25e31f941381bdddd8c93f121df13dad9cea4
joel <angeljoeltoledo@gmail.com>
2026-05-21 14:27:24 -0300

    Resolucion Diccionario y List

--- Dsw2026Ej11/Collections/CasoDictionary.cs
+++ Dsw2026Ej11/Collections/CasoDictionary.cs
@@ -1,4 +1,6 @@
-﻿namespace Dsw2026Ej11.Collections;
+﻿using Dsw2026Ej11.Domain;
+
+namespace Dsw2026Ej11.Collections;
 
 //Crear un diccionario donde la clave sea el legajo y el valor el alumno
 //Incluir un método para agregar un alumno al diccionario
@@ -7,4 +9,34 @@
 //Incluir un método para eliminar un alumno utilizando la clave
 public class CasoDictionary
 {
+    //Supondre que el id es el legajo
+    private Dictionary<int, Alumno> DiccionarioAlumnos;
+    public CasoDictionary()
+    {
+        DiccionarioAlumnos = new Dictionary<int, Alumno>();
+    }
+
+
+    public void AgregarAlumno(Alumno alumno)
+    {
+        DiccionarioAlumnos?.Add(alumno.Id, alumno);
+    }
+
+    public Alumno? BuscarAlumno(int legajo)
+    {
+        return DiccionarioAlumnos?.GetValueOrDefault(legajo);
+    }
+    
+    public Dictionary<int, Alumno>? RetornarDiccionario()
+    {
+        return DiccionarioAlumnos;
+    }
+
+    public void EliminarAlumno(int legajo)
+    {
+        DiccionarioAlumnos?.Remove(legajo);
+    }
+
 }
+
+
--- Dsw2026Ej11/Tests/Ejemplos.cs
+++ Dsw2026Ej11/Tests/Ejemplos.cs
@@ -1,4 +1,7 @@
-﻿namespace Dsw2026Ej11.Tests;
+﻿using Dsw2026Ej11.Collections;
+using Dsw2026Ej11.Domain;
+
+namespace Dsw2026Ej11.Tests;
 
 
 internal class Ejemplos
@@ -11,7 +14,53 @@ internal class Ejemplos
     //Eliminar el primer elemento de la lista y listar por consola los alumnos
     public static void EjemploList()
     {
+        CasoList CasoList = new CasoList();
+        CasoList.AgregarAlumno(new Alumno(1, "Juan", 8.5));
+        CasoList.AgregarAlumno(new Alumno(2, "Manuel", 8));
+        CasoList.AgregarAlumno(new Alumno(3, "Rol", 7.5));
 
+
+        Console.WriteLine("listaAlumnos");
+        var listaAlumnos = CasoList.RetornarListaAlumnos();
+        foreach (var alumno in listaAlumnos)
+        {
+            Console.WriteLine(alumno);
+        }
+
+
+        Console.WriteLine("\n Alumno buscado:");
+        Alumno alumnoBuscado = CasoList.BuscarAlumno("Juan");
+        Console.WriteLine(alumnoBuscado);
+
+        Console.WriteLine("\n Alumno buscado2:");
+        Alumno alumnoBuscado2 = CasoList.BuscarAlumno("Maria");
+        if (alumnoBuscado2 != null) 
+            Console.WriteLine(alumnoBuscado2);
+        else
+            Console.WriteLine("No existe");
+
+
+        Alumno alumnoEliminado = CasoList.BuscarAlumno("Manuel");
+        CasoList.EliminarAlumno(alumnoEliminado);
+
+        Console.WriteLine("\n listaAlumnosModificadaPorEliminacion");
+        listaAlumnos = CasoList.RetornarListaAlumnos();
+        foreach (var alumno in listaAlumnos)
+        {
+            Console.WriteLine(alumno);
+        }
+
+
+        //Alumno alumnoEliminado2 = CasoList.BuscarAlumno("Juan");
+        //int indexAlumnoEliminado2 = CasoList.RetornarListaAlumnos().IndexOf(alumnoEliminado2);
+        CasoList.EliminarAlumnoPorPosicion(0);
+
+        Console.WriteLine("\n listaAlumnosModificadaPorEliminacionPorPosicion");
+        listaAlumnos = CasoList.RetornarListaAlumnos();
+        foreach (var alumno in listaAlumnos)
+        {
+            Console.WriteLine(alumno);
+        }
     }
 
     //Agregar 3 alumnos al diccionario
@@ -21,12 +70,50 @@ internal class Ejemplos
     //Eliminar un alumno por clave y listar por consola los alumnos
     public static void EjemploDictionary()
     {
+        CasoDictionary CasoDictionary = new CasoDictionary();
+        CasoDictionary.AgregarAlumno(new Alumno(1, "Juan", 8.5));
+        CasoDictionary.AgregarAlumno(new Alumno(2, "Manuel", 8));
+        CasoDictionary.AgregarAlumno(new Alumno(3, "Rol", 7.5));
+
+        Console.WriteLine("DiccionarioAlumnos:");
+        var diccionarioAlumnos = CasoDictionary.RetornarDiccionario();
+        foreach (var alumno in diccionarioAlumnos)
+        {
+            Console.WriteLine(alumno.Value);
+        }
+
+        
+        Console.WriteLine("\n Alumno buscado:");
+        var alumnoBuscado = CasoDictionary.BuscarAlumno(1);
+        Console.WriteLine(alumnoBuscado);
+
+        Console.WriteLine("\n Alumno buscado:");
+        var alumnoBuscado2 = CasoDictionary.BuscarAlumno(10);
+        if (alumnoBuscado2 != null)
+            Console.WriteLine(alumnoBuscado2);
+        else
+            Console.WriteLine("No existe");
+
+
+
+        Console.WriteLine("\n DiccionarioAlumnosModificadoPorEliminacion:");
+        CasoDictionary.EliminarAlumno(2);
+
+        var diccionarioAlumnos2 = CasoDictionary.RetornarDiccionario();
+        foreach (var alumno in diccionarioAlumnos)
+        {
+            Console.WriteLine(alumno.Value);
+        }
+
+
 
     }
 
     //Realizar una llamada a cada método definido en CasoLinq y mostar por consola según corresponda
     public static void EjemploLinq()
     {
+
+        
 
     }
 }
